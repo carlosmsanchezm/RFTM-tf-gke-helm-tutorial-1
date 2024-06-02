59c10a32ee3aceb56dc84cdb3d9e1b172556d71f
carlosmsanchezm <carlos.sanchez.m.m@gmail.com>
2024-06-02 03:57:15 -0400

    new app to fit linux/amd arch

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM python:3.8
+FROM python:3.8-slim
 
-COPY . /app
 WORKDIR /app
 
+COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-CMD ["python", "-u", "server.py"]
+COPY . .
+
+CMD ["python", "server.py"]
+
--- server.py
+++ server.py
@@ -1,19 +1,17 @@
-import tornado.ioloop
-import tornado.web
+from flask import Flask, request, render_template
+import redis
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        title = "Hello, World!"
-        bgcolor = "dodgerblue"
-        self.render("template.html", title=title, bgcolor=bgcolor)
-        print(self.request)
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        r.lpush('names', name)
+    names = r.lrange('names', 0, -1)
+    return render_template('index.html', names=names)
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    app.run(host='0.0.0.0', port=8888)
+
